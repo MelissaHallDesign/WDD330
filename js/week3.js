80b8eae0834816ed560ae5be8cbc4e22fca18ad1
melissahalldesign <hallfamily@live.com>
2020-05-09 15:06:22 -0600

    updated week 3 notes

--- js/week3.js
+++ js/week3.js
@@ -77,11 +77,34 @@
 
       // Array.prototype.sort()
       // 3. Sort the inventors by birthdate, oldest to youngest
+      /*
+      const ordered = inventors.sort(function(firstPerson, lastPerson) {
+        if(firstPerson.year > lastPerson.year) {
+            return 1;
+        } else {
+            return -1;
+        }
+      });
+      */
+      const ordered = inventors.sort((firstPerson, lastPerson) => firstPerson.year > lastPerson.year ? 1 : -1);
+      console.table(ordered);
 
       // Array.prototype.reduce()
       // 4. How many years did all the inventors live?
+      const totalYears = inventors.reduce((total, inventor) => {
+          return total + (inventor.passed - inventor.year);
+      }, 0);
+      console.log(totalYears);
+      
 
       // 5. Sort the inventors by years lived
+      const oldest = inventors.sort(function(a, b) {
+          const lastGuy = a.passed - a.year;
+          const nextGuy = b.passed - b.year;
+          return lastGuy > nextGuy ? -1 : 1;
+      });
+      console.table(oldest);
+
 
       // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
       // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
