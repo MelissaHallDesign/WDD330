871a17c16527b82f7f9359b3c56ab3b04728ead5
melissahalldesign <hallfamily@live.com>
2020-07-07 19:03:21 -0600

    updated json and js for hike list

--- HikeBikeGo/js/hbgmain.js
+++ HikeBikeGo/js/hbgmain.js
@@ -3,6 +3,11 @@ function toggleMenu() {
 }
 document.querySelector('#menu').addEventListener('click', toggleMenu);
 
+// function toggleMenu() {
+//   document.querySelector('#showHike').classList.toggle('openDetails');
+// }
+// document.querySelector('#details').addEventListener('click', toggleMenu);
+
 /*
 Flaming Gorge (Manila UT) https://openweathermap.org/city/5777855
 Bear Lake (Garden City UT) https://openweathermap.org/weathermap?basemap=map&cities=true&layer=temperature&lat=41.9605&lon=-111.3986&zoom=10
@@ -25,10 +30,12 @@ lat, lon coordinates of the location of your interest
 Examples of API calls:
 api.openweathermap.org/data/2.5/weather?lat=35&lon=139
 */
-$(document).ready(function() {
+
+
+// $(document).ready(function() {
   
-    $(".showDetails dt a").click(function(){
-      //alert("click")
-      $(this).parent().next('dd').toggle();
-    });
-  });
+//     $(".showDetails dt a").click(function(){
+//       //alert("click")
+//       $(this).parent().next('dd').toggle();
+//     });
+//   });
